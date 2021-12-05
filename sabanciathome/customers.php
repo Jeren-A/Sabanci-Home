20312ce4608caef9303a0c30e34a9ae89c59c09d
Ali Parlakci <parlakciali@gmail.com>
2021-12-05 19:45:51 +0300

    Customers

--- sabanciathome/customers.php
+++ sabanciathome/customers.php
@@ -5,39 +5,46 @@ include "header.php";
 
 ?>
 
-<a href="insertcustomer.php">Add customer</a>
-
 <?php
+
 $sql_statement = "SELECT * FROM `customer`";
 $result = mysqli_query($db, $sql_statement); // Executing query
 $customers = array();
 while ($row = mysqli_fetch_assoc($result)) { // Fetch and iterate the result
     array_push($customers, $row);
 }
-?>
 
-<table class="table">
-    <thead>
-        <tr>
-            <th></th>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($customers as $customer): ?>
+?>
+<div>
+    <h1 class="text-3xl font-bold my-2 w-100 text-center">Customers</h1>
+</div>
+<div id="container" class="flex">
+    <div id="sidebar" class="flex flex-col gap-3 p-3" style="min-width: 15rem;">
+        <a href="insertcustomer.php" class="w-full text-center">New customer</a>
+    </div>
+    <table class="table">
+        <thead>
             <tr>
-                <td>
-                    <form action="deletecustomer.php" method="POST">
-                        <input type="hidden" value="<?php echo $customer['user_id'] ?>" name="id" />
-                        <input type="submit" class="btn btn-danger" value="X" />
-                    </form>
-                </td>
-                <td><?php echo $customer['user_id']; ?></td>
-                <td><?php echo $customer['display_name']; ?></td>
-                <td><?php echo $customer['email']; ?></td>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th></th>
             </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php foreach ($customers as $customer) : ?>
+                <tr>
+                    <td><?php echo $customer['user_id']; ?></td>
+                    <td><?php echo $customer['display_name']; ?></td>
+                    <td><?php echo $customer['email']; ?></td>
+                    <td class="text-center">
+                        <form action="deletecustomer.php" method="POST">
+                            <input type="hidden" value="<?php echo $customer['user_id'] ?>" name="id" />
+                            <input type="submit" class="btn btn-danger rounded-full text-xs" value="Delete" />
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
--- sabanciathome/insertcustomer.php
+++ sabanciathome/insertcustomer.php
@@ -9,9 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $display_name = $_POST['display_name'];
         $email = $_POST['email'];
         $business_type = $_POST['business_type'];
-        $flop_demand = 0;
-        $problem_count = 0;
-        $sql_statement = "INSERT INTO `customer` (`email`, `passphrase`, `display_name`, `business_type`, `flop_demand`, `problem_count`) VALUES ('$email','$passphrase','$display_name','$business_type','$flop_demand','$problem_count')";
+        $sql_statement = "INSERT INTO `customer` (`email`, `passphrase`, `display_name`, `business_type`) VALUES ('$email','$passphrase','$display_name','$business_type')";
         $result = mysqli_query($db, $sql_statement);
         if ($result) {
             echo "<a href=\"customers.php\">Succesful! Return back</a>";
@@ -23,24 +21,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 ?>
 
-<form class="form-control" id="form" action="insertcustomer.php" method="POST">
-    email:
-    <input type="text" class="form-control" id="email" name="email">
-    <br />
-    passphrase:
-    <input type="password" class="form-control" id="passphrase" name="passphrase">
-    <br />
-    display_name:
-    <input type="text" class="form-control" id="display_name" name="display_name">
-    <br />
-    business_type:
-    <input type="text" class="form-control" id="business_type" name="business_type">
-    <br />
-    flop_demand:
-    <input type="number" class="form-control" id="flop_demand" name="flop_demand">
-    <br />
-    problem_count:
-    <input type="number" class="form-control" id="problem_count" name="problem_count">
-    <br />
-    <input type="submit" class="btn btn-primary" value="Submit">
-</form>
+<body class="w-full">
+
+    <form class="form-control w-96 mx-auto mt-4 p-3" id="form" action="insertcustomer.php" method="POST">
+        <div class="text-2xl font-light mb-3 w-full flex justify-center">New customer</div>
+        <div class="grid grid-cols-2 gap-3">
+            <label for="display_name">Full name</label>
+            <input type="text" class="form-control" id="display_name" name="display_name">
+
+            <label for="email">Email</label>
+            <input type="text" class="form-control" id="email" name="email">
+
+            <label for="passphrase">Password</label>
+            <input type="password" class="form-control" id="passphrase" name="passphrase" autocomplete="new-password">
+            
+            <label for="business_type">Business Type:</label>
+            <select name="business_type" id="business_type">
+                <option>For profit</option>
+                <option>Non-profit</option>
+                <option>Other</option>
+            </select>
+        </div>
+        <input type="submit" class="btn btn-primary w-full mt-3" value="Create">
+    </form>
+    </form>
+</body>
