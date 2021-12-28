4906d69b6b030026007d69e64c4534b45e431932
Ali Parlakci <parlakciali@gmail.com>
2021-12-28 22:32:53 +0300

    deleteproblem.php deletes the chunks also

--- sabanciathome/deleteproblem.php
+++ sabanciathome/deleteproblem.php
@@ -5,9 +5,19 @@ include "config.php";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!empty($_POST['id'])) {
         $id = $_POST['id'];
-        $sql_statement = "DELETE FROM `problem` WHERE problem.problem_id = $id";
-        echo $sql_statement;
-        $result = mysqli_query($db, $sql_statement);
+        try {
+            $db->begin_transaction();
+            
+            $db->query("DELETE FROM chunk WHERE chunk.chunk_id IN (SELECT problemchunk.chunk_id FROM problemchunk WHERE problemchunk.problem_id = $id)");
+            $db->query("DELETE FROM problem WHERE problem.problem_id = $id");
+
+            $db->commit();
+
+            $result = true;
+        } catch (Throwable $e) {
+            $db->rollback();
+            $result = false;
+        }
         if ($result) {
             echo "<a href=\"/sabanciathome/problems.php\">Return to Problem</a>";
         }
