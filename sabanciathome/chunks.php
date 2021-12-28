89e503ad6982064aefb11ead692952b9ab9ebfe5
AlbertDenizLevi <78790996+AlbertDenizLevi@users.noreply.github.com>
2021-12-28 18:25:20 +0300

    added main page, changed the default colors of background and text color

--- sabanciathome/chunks.php
+++ sabanciathome/chunks.php
@@ -2,7 +2,6 @@
 
 include "config.php";
 include "header.php";
-
 ?>
 
 <?php
--- sabanciathome/index.php
+++ sabanciathome/index.php
@@ -1,1 +1,36 @@
-hello!!
+<?php
+
+include "config.php";
+include "header.php";
+
+?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+<body style='background-color:Thistle'>
+        
+
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-lg-12">
+            <h1 style="font-size:100px;font-size: 10vmin;margin:15px 0px 5px 0px;color:DarkSlateBlue;">S@H</h1>
+                <p>
+                <a href="customers.php" class="btn btn-secondary btn-square-xl btn-dark" style="background-color: DarkSlateBlue;" role="button" aria-pressed="true"><br /><i class="fa fa-university fa-5x "> </i><br /><br />Add, Delete And view Customers<br /><br /></a>
+            
+        
+                <a href="donators.php" class="btn btn-secondary btn-square-xl btn-dark" style="background-color: DarkSlateBlue;" role="button" aria-pressed="true"><br /><i class="fa fa-users fa-5x"> </i><br /><br /> Add Delete and view Donators<br /><br /></a>
+                
+            
+                <a href="chunks.php" class="btn btn-secondary btn-square-xl btn-dark" style="background-color: DarkSlateBlue;" role="button" aria-pressed="true"><br /><i class="fa fa-puzzle-piece fa-5x"> </i><br /><br />Add Delete and view Chunks<br /><br /></a>
+                
+            
+                <a href="problems.php" class="btn btn-secondary btn-square-xl btn-dark" style="background-color: DarkSlateBlue;" role="button" aria-pressed="true"><br /><i class="fa fa-calculator fa-5x"> </i><br /><br />Add Delete and view Problems<br /><br /></a>
+                </p>
+                <h1 style="font-size:100px;font-size: 10vmin;margin:15px 0px 5px 0px;color:DarkSlateBlue">A CS306 Project<br /></h1>
+                <button type="button" class="btn btn-default btn-circle" style="background-color: DarkSlateBlue;"><i class="fa fa-database fa-5x text-white"></i></button>
+                
+            
+                </div>
+        </div>
+    </div>
